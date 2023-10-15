1b8a676052a163c3ed93c283b0686658f4d524ab
Shad <144332050+Shadrachanwhere@users.noreply.github.com>
2023-10-16 00:52:22 +0100

    updating

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from constants import PLAYERS
+import constants
+from constants import formatted_data
+
+Panthers, Bandits, Warriors = constants.balanced_team(formatted_data)
 
 
-def cleaned_data(data):
-    cleaned = []
-    for player in PLAYERS:
-        fixed = {}
-        fixed["name"]  = player["name"]
-        fixed[]
+def app_basket_ball():
+    print("-_-_  MENU -_-_-_\n\n\n A) Display Team Stats\n B) Quit\n")
 
+    option = input("Enter an option: ").lower()
+    if option == "a":
+        print("-_-_ TEAMS -_-_\n\n 1) Panthers\n 2) Bandits\n 3) Warriors")
+    else:
+        print("Thank you for using this app! Goodbye")
+
+    team_select = input("Enter an option: ")
+    if team_select == "1":
+        constants.stats(Panthers)
+    elif team_select == "2":
+        constants.stats(Bandits)
+    else:
+        constants.stats(Warriors)
+    continue_app = input("Press enter to continue: ")
+    if continue_app == "":
+        app_basket_ball()
+
+
+app_basket_ball()
--- constants.py
+++ constants.py
@@ -5,22 +5,22 @@ TEAMS = [
 ]
 
 PLAYERS = [{
-        'name': 'Karl Saygan',
-        'guardians': 'Heather Bledsoe',
-        'experience': 'YES',
-        'height': '42 inches'
-    },
+    'name': 'Karl Saygan',
+    'guardians': 'Heather Bledsoe',
+    'experience': 'YES',
+    'height': '42 inches'
+},
     {
         'name': 'Matt Gill',
         'guardians': 'Charles Gill and Sylvia Gill',
         'experience': 'NO',
         'height': '40 inches'
     },
-    {   'name': 'Sammy Adams',
-        'guardians': 'Jeff Adams and Gary Adams',
-        'experience': 'NO',
-        'height': '45 inches'
-    },
+    {'name': 'Sammy Adams',
+     'guardians': 'Jeff Adams and Gary Adams',
+     'experience': 'NO',
+     'height': '45 inches'
+     },
     {
         'name': 'Chloe Alaska',
         'guardians': 'David Alaska and Jamie Alaska',
@@ -112,3 +112,67 @@ PLAYERS = [{
         'height': '41 inches'
     }
 ]
+
+
+def cleaned_data(players):  ## a function that cleans the Players list data
+    cleaned = []
+    for player in players:
+        fixed = {}
+        fixed["name"] = player["name"]
+        fixed["guardians"] = player["guardians"].split("and")
+        if player["experience"] == "YES":
+            fixed["experience"] = True
+        else:
+            fixed["experience"] = False
+        fixed["height"] = int(player["height"].split(" ")[0])
+        cleaned.append(fixed)
+    return cleaned
+
+
+formatted_data = cleaned_data(PLAYERS)  ## cleaned data is now stored in formatted_data
+
+
+def balanced_team(b_team):  # a function to evenly balance the players across the team
+    P = b_team[0:5]
+    B = b_team[6:12]
+    W = b_team[13:18]
+    return P, B, W
+
+
+def stats(team):  # a function to analyse the statistics of the player
+    guardian_list = []
+    player_name_list = []
+    height_list = []
+    count_experience = 0
+    count_inexperience = 0
+    for user in team:
+        player_name = user["name"]
+        guardian = user["guardians"]
+        height = user["height"]
+        player_name_list.append(player_name)
+        guardian_list.append(guardian)
+        height_list.append(height)
+        if user["experience"]:
+            count_experience += 1
+        else:
+            count_inexperience += 1
+    average_height = int(sum(height_list) / len(height_list))
+    number_of_players = len(player_name_list)
+    number_of_experience = count_experience
+    number_of_inexperience = count_inexperience
+    print(f"Total Players: {number_of_players}")
+    print(f"Number of experience: {number_of_experience}")
+    print(f"Number of inexperience: {number_of_inexperience}")
+    print(f"Average height: {average_height}")
+    print(player_name_list)
+    print(guardian_list)
+
+
+def main():
+    cleaned_data(PLAYERS)
+    stats(team)
+    balanced_team(f_data)
+
+
+if __name__ == "__main__":
+    main()
